b25ab98053be665e54e0c279e556edb450704581
zuharz <105288148+zuharz@users.noreply.github.com>
2023-01-21 12:21:09 +0200

    Menu model unit tests added for demo purposes.

--- littlelemon/tests/test_models.py
+++ littlelemon/tests/test_models.py
@@ -1,11 +1,33 @@
 from django.test import TestCase
 from restaurant.models import Menu
+from decimal import Decimal
 
 class MenuTest(TestCase):
     def setUp(self) -> None:
-        self._menu_itemm_title = 'Ice Cream'
-        Menu.objects.create(title=f'{self._menu_itemm_title}', price=12.99, inventory=100)
-    
-    def test_add_menu_item(self) -> None:
-        self.menu_item = Menu.objects.get(title=f'{self._menu_itemm_title}')
-        self.assertEqual(str(self.menu_item), f'{self._menu_itemm_title} : 12.99')
+        self.item1 = Menu.objects.create(
+            title = 'Pizza',
+            price = 12.99,
+            inventory = 10
+        )
+
+    def test_create_menu_item(self) -> None:
+        item2 = Menu.objects.create(
+            title = 'Burger',
+            price = 17.99,
+            inventory = 5
+        )
+        self.assertEqual(Menu.objects.count(), 2)
+        self.assertEqual(item2.title, 'Burger')
+        self.assertEqual(item2.price, Decimal(17.99))
+        self.assertEqual(item2.inventory, 5)
+
+    def test_get_menu_item(self) -> None:
+        item = Menu.objects.get(id = self.item1.id)
+        self.assertEqual(item.title, 'Pizza')
+        self.assertEqual(item.price, Decimal(12.99).quantize(Decimal("0.00")))
+        self.assertEqual(item.inventory, 10)
+
+    def test_delete_menu_item(self) -> None:
+        item = Menu.objects.get(id = self.item1.id)
+        item.delete()
+        self.assertEqual(Menu.objects.count(), 0)
