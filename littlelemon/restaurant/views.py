9468053c83270719e69ae5a80912f8acfc746fbf
zuharz <105288148+zuharz@users.noreply.github.com>
2023-01-22 20:32:03 +0200

    views from the full-stack course project added

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,15 +1,17 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.core import serializers
 from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from datetime import datetime
 
 from .serializers import BookingSerializer, MenuSerializer
 from .models import Booking, Menu
 
-def index(request: HttpRequest) -> HttpResponse:
-    return render(request, 'index.html', {})
-
+# 
+# API views
+#
 class MenuItemsView(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated]
     
@@ -24,4 +26,17 @@ class BookingViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     
     queryset = Booking.objects.all()
-    serializer_class = BookingSerializer
+#
+# Page views
+#
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, 'index.html', {})
+
+def about(request):
+    return render(request, 'about.html')
+
+def reservations(request:HttpRequest) -> HttpResponse:
+    date = request.GET.get('date',datetime.today().date())
+    bookings = Booking.objects.all()
+    booking_json = serializers.serialize('json', bookings)
+    return render(request, 'reservations.html',{"bookings":booking_json})
